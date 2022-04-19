52594a2284ce54891ae98fd67c69518d8479fb0e
jskim676 <wkxndlsvk@gmail.com>
2022-04-19 17:57:12 +0900

    PM 05:57

---
+++ javascript/test.js
@@ -0,0 +1,14 @@
+
+function sum(a,b) {
+  // 매개변수 a와 b는 값이 들어갈 공간이다.
+  // 함수는 상자라서 열방법이 없다.
+  return a + b;
+  // 반환 = 뱉어낸다.
+}
+
+console.log(sum(9, 1));
+console.log(sum(1123, 1232));
+console.log(sum(9123, 4521));
+console.log(sum(92, 11));
+console.log(sum(69, 12));
+
