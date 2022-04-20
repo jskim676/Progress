0e20dcee245b664c2d86e0e0ef6375db32322731
jskim676 <wkxndlsvk@gmail.com>
2022-04-20 17:54:24 +0900

    PM 05:54

---
+++ javascript/game.js
@@ -0,0 +1,18 @@
+
+    var today = new Date();
+    var day = today.getDay();
+
+    console.log(day);
+
+
+    let game = {
+      1 : "lol",
+      2 : "steam",
+      3 : "apexlegend",
+      4 : "pubg",
+      5 : "maple"
+    }
+
+    console.log(game.console.log(day));
+
+
