2d700750e2d02798ffb0e9de5ba23caad6d15c7c
jskim676 <wkxndlsvk@gmail.com>
2022-04-20 17:00:01 +0900

    PM 05:00

--- javascript/example.js
+++ javascript/example.js
@@ -1,1 +1,60 @@
-console.log('hello');
+
+// let one = "1";
+// // 변수를 선언할께 이름은 one 이야, 선언한 김에 값도 대입해줄게 그 값은 숫자타입의 1이야
+
+// let two = 1;
+
+// console.log(one);
+// // 자바스크립트야 변수 one이 뭔지 모르겠는데, 니 실행창에 기록좀 남겨줘
+// console.log(typeof one);
+// // 자바스크립트야 변수 one이 데이터타입이 뭔지 모르겠어 이거 뭐야?
+
+// let one = "1";
+// let two = 1;
+// console.log(typeof one + two);
+
+// let father = "아버지가" ;
+// let room = "방에들어가신다.";
+
+// console.log(typeof (father + room) );
+// // 자바스크립트야 father라는 변수와 room 이라는 변수를 더한 다음에 데이터 타입 좀 체크해줄래?
+
+
+// let na = "공욱재는 미남이다.";
+
+// if(typeof na === "string") {
+//   // if 만약에 자바스크립트야, 소괄호에 있는 내용이 맞으면 중괄호에 있는것좀 실행 시켜줘
+//   console.log("아하하하하하하하");
+// }
+
+
+// let sohi = {
+//   name : "sohi",
+//   gender : "female"
+// }
+
+// console.log(typeof sohi);
+// if(sohi.name === "ukjae") {
+//   console.log("미남이야");
+// } else {
+//   console.log("틀렸어 임마");
+// }
+
+// let a = 1;
+// let b = 2;
+// let result = "값이 다릅니다."
+// if (a === b) {
+//   console.log('값이 같습니다.');
+// } else {
+//   console.log(result);
+//   // 거짓인 경우 변수인 result를 기록하는 행동만 '할 뿐이다' 변수의 값이 무엇인지 관심이 없다.
+// }
+
+
+let a = 1;
+let b = 2;
+if (a !== b) {
+  console.log('값이 같습니다.');
+} else {
+  console.log('값이 다릅니다.');
+}
