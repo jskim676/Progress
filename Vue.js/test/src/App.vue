cb5bb3b72ffcbe4b7da6c4a2ec7e652d41acb254
Kim Jun Sik <wkxndlsvk@gmail.com>
2022-07-17 22:29:19 +0900

    Vue.js setting

--- Vue.js/test/src/App.vue
+++ Vue.js/test/src/App.vue
@@ -1,95 +1,26 @@
 <template>
-
-  <div class="menu">
-    <a v-for="a in menu" :key="a">{{a}}</a>
-  </div>
-
-  <div>
-    <img src="./assets/room0.jpg" alt="" class="room-img">
-    <h4>{{원룸들[0].title}}</h4>
-    <p>{{원룸들[0].price}}</p>
-  </div>
-
-
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-
 <script>
-
-import data from './assets/oneroom.js'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      원룸들 : data,
-      모달창 : false,
-      신고수 : [0,0,0],
-      menu : ['Home', 'Shop', 'About'],
-      products : ['역삼동 원룸', '천호동 원룸', '마포구원룸'],
-    }
-  },
-
-  methods : {
-    increase () {
-      this.신고수 += 1;
-    }
-  },
-
-  // components: {
-    
-  // }
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
-
-body {
-  margin:0;
-}
-
-div {
-  box-sizing: border-box;
-}
-
-.black-bg {
-  width:100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position:fixed; padding:20px;
-}
-
-.white-bg {
-  width:100%; background: white;
-  border-radius: 8px;
-  padding:20px;
-}
-
-
-.room-img {
-  width:100%;
-  margin-top:40px;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-.menu {
-  background:darkslateblue;
-  padding:15px;
-  border-radius: 5px;
-}
-
-.menu a {
-  color : white;
-  padding: 10px;
-}
-
-
-
 </style>
